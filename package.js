fa6a793ea61ec59aff0f418402df62c4f5ba91f2
NoahVlncrt <noah0000games@gmail.com>
2016-01-08 07:53:06 -0500

    changed package version to match source

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
 	name: 'milligram:milligram',
 	summary: 'A new minimal css framework that provides an easy starting point.',
-	version: '1.0.6',
+	version: '1.0.3',
 	git: 'https://github.com/milligram/milligram'
 });
 
