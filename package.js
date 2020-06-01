77d5b5082210ee8f721efd202ed213f86a200f31
CJ Patoilo <cjpatoilo@gmail.com>
2020-06-01 18:54:21 -0300

    Build new release v1.4.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'milligram:milligram',
-  version: '1.3.0',
+  version: '1.4.0',
   summary: 'A minimalist CSS framework.',
   git: 'https://github.com/milligram/milligram.git',
   documentation: 'readme.md',
